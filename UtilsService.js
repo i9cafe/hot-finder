2fae4b74150c720044b62be28aaa672a5d3bf864
i9cafe <41662531+i9cafe@users.noreply.github.com>
2025-11-18 12:49:34 +0900

    Update UtilsService.js

--- UtilsService.js
+++ UtilsService.js
@@ -12,6 +12,16 @@ angular.module('hotFinder')
         document.getElementById('content').style.display = 'block';
       };
 
+  this.getKoreaTimeFromPacificMidnight = () => {
+				const n = new Date(), y = n.getUTCFullYear(),
+		        s = new Date(Date.UTC(y,2,8 + (7-new Date(Date.UTC(y,2,8)).getUTCDay())%7,10)),
+		        e = new Date(Date.UTC(y,10,1 + (7-new Date(Date.UTC(y,10,1)).getUTCDay())%7,9)),
+		        o = (n >= s && n < e ? -7 : -8),
+		        k = new Date(Date.UTC(y,n.getUTCMonth(),n.getUTCDate(),-o,0,0)+9*3600*1000),
+		        h = k.getUTCHours();
+		  return `${h>=12?'오후':'오전'} ${h%12||12}시`; 
+      };
+
   
 
 }]);
