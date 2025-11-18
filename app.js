8f1197738c2a6869c77a3bccc58c54df6d3cdd80
i9cafe <41662531+i9cafe@users.noreply.github.com>
2025-11-18 10:49:46 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1458,47 +1458,7 @@ const app = angular.module('hotFinder', ['ngRoute'
 		}
 	])
 
-  .controller('IndexController', [
-    '$scope',
-    function($scope) {
 
-      /*************************** DEFAULT INFO SETUP ***************************/
-      const vm = this;
-
-  	  let today = new Date();
-  	  let year = today.getFullYear(); // 년도
-  	  let month = today.getMonth() + 1; // 월 (0부터 시작하므로 1 더함)
-  	  let day = today.getDate(); // 일
-
-  	  const today_MMdd = String(month).padStart(2, '0') + String(day).padStart(2, '0');
-
-      const USER_ID = 'gw' + today_MMdd;
-      const USER_PW = 1203;
-
-      /*************************** FUNCTION LIST ***************************/
-      this.login = () => {
-
-        const id = document.getElementById("id");
-        const pw = document.getElementById("pw");
-
-        if (String(id.value) === null || String(id.value).trim() === '') {
-          alert("Please Enter ID");
-          return;
-        } else if (String(pw.value) === null || String(pw.value).trim() === '') {
-          alert("Please Enter Password");
-          return;
-        } else if (String(id.value).toLowerCase() !== USER_ID || Number(pw.value) !== USER_PW) {
-
-          alert("Login Failed");
-          return;
-
-        } else {
-          location.href = "main.html";
-        }
-      };
-
-    }
-  ])
 
 
 
