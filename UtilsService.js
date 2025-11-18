d91e0bad8692b911b138ee740f7e230ab403403e
i9cafe <41662531+i9cafe@users.noreply.github.com>
2025-11-18 12:33:49 +0900

    Update UtilsService.js

--- UtilsService.js
+++ UtilsService.js
@@ -1,1 +1,17 @@
 
+angular.module('hotFinder')
+.service('UtilsService', ['$timeout', '$uibModal', function($timeout, $uibModal) {
+
+  this.showLoader = () => {
+        document.getElementById('loader').style.display = 'flex';
+        document.getElementById('content').style.display = 'none';
+      };
+
+      this.hideLoader = () => {
+        document.getElementById('loader').style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+      };
+
+  
+
+}]);
